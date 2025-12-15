9486fcd34479e98bf4701331c96b0a776d6f5459
dFegnoux <denis.fegnoux@proton.me>
2025-12-15 23:14:23 +0100

    feat: add task when submit form #2

--- todolist.js
+++ todolist.js
@@ -1,16 +1,34 @@
 addEventListener("load", (event) => {
   // Retrieve elements from DOM
-  const addItemButton = document.getElementById('add-item')
-  const todoContainer = document.getElementById('todo')
-  const addItemInput = document.getElementById('new-todo-text')
 
-  // Handle click
-  addItemButton.addEventListener('click', () => {
+  function createTaskItem(text) {
+    // Create new list item element
+    const listItem = document.createElement('li')
+    
+    // Put taskText into the list item
+    listItem.innerText = text
+
+    return listItem
+  }
+
+  function onTaskSubmit(event) {
+    // prevent default behavior of form submission to avoid page to reload with params
+    event.preventDefault() 
+
+    // Get current value in text input
+    const taskTitleInput = document.getElementById('task-title')
+
     // Create new element
-    const newTodoItem = document.createElement('li')
-    newTodoItem.innerText = addItemInput.value
+    const newTodoItem = createTaskItem(taskTitleInput.value)
 
     // Add new element to list container
+    const todoContainer = document.getElementById('task-list')
     todoContainer.append(newTodoItem)
-  })
+  
+    // Reset input
+    taskTitleInput.value = ''
+  }
+
+  // Handle form submission
+  document.addEventListener('submit', onTaskSubmit)
 })
